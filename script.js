23c2d32f3df661a70f836e21107e4a075cc1be72
abindran <20it001@skcet.ac.in>
2021-05-14 05:57:37 +0530

    final project

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-questionAnswers={
-    "hi":"hi buddy!!!!",
-}
-
-
-
-document.querySelector("#queries").addEventListener("keypress", (event)=>{
-if(event.key==='Enter'){
-    talk(event)
-}
-} )
-
-const talk=(event)=>{
-   // var question = document.getElementById("queries").value;
-    console.log(event)
-   var question = event.target.value;
-    if (question in questionAnswers){
-        document.getElementById("reply").innerHTML+=questionAnswers[question] + "<br>";
+var progression=0;
+function checkingSymptoms(target){
+    // var checkBox = document.getElementById("mycheck");
+    if(target.checked===true){
+        progression+=1;
     }else{
-        document.getElementById("reply").innerHTML+="Can't Understand..."+"<br>";
+        progression-=1;
+    }
+    progressBar();
+}
+
+const progressBar=()=>{
+    var ele=document.getElementById("coloring");
+    var ele1=document.getElementById("reply");
+    if (progression===0){
+        document.getElementsByClassName("progress-bar")[0].setAttribute("style", "width:0%");
+        document.getElementById("reply").innerHTML=" ";
+    }else if(progression===1){
+        document.getElementsByClassName("progress-bar")[0].setAttribute("style", "width:20%");
+        ele.classList.add("bg-success");
+        ele.classList.remove("bg-danger");
+        ele.classList.remove("bg-warning");
+       document.getElementById("reply").innerHTML="Don't Worry Stay Home!!! Stay Safe !!!";
+       ele1.classList.add("green");
+       ele1.classList.remove("red");
+       ele1.classList.remove("bold");
+    }else if(progression===2){
+        document.getElementsByClassName("progress-bar")[0].setAttribute("style", "width:40%");
+    }else if(progression===3){
+        document.getElementsByClassName("progress-bar")[0].setAttribute("style", "width:60%");
+        document.getElementById("reply").innerHTML="Must Consult a Doctor!!!";
+        ele.classList.remove("bg-success");
+        ele.classList.add("bg-warning");
+        ele.classList.remove("bg-danger");
+        ele1.classList.add("red");
+        ele1.classList.remove("green");
+        ele1.classList.remove("bold");
+    }else if(progression===4){
+        document.getElementsByClassName("progress-bar")[0].setAttribute("style", "width:80%");
+    }else if(progression===5){
+        document.getElementsByClassName("progress-bar")[0].setAttribute("style", "width:100%");
+        ele.classList.remove("bg-warning");
+        ele.classList.add("bg-danger");
+        ele1.classList.add("red");
+        ele1.classList.add("bold");
+        ele1.classList.remove("green");
     }
 }
